f2e5dbeb3f2988f7077be84fad989f24a86825d0
Nikita Budyldin <budnalex@yandex.ru>
2025-11-09 23:36:12 +0300

    А если так?

--- tests/test_get_order_list.py
+++ tests/test_get_order_list.py
@@ -10,7 +10,18 @@ class TestOrdersList:
         assert response_status.status_code == 200 and Flags.SUCCESSFUL_GET_ORDER_LIST in response_status.json()
 
     @allure.title('Test get order list with certain metro. Handle:/api/v1/orders')
-    def test_successful_get_order_list_with_certain_metro(self):
+    def test_successful_get_order_list_with_certain_metro(self, create_order_metro_1, create_order_metro_2):
+        order_data = [create_order_metro_1, create_order_metro_2]
         params={"nearestStation": '["1","2"]'}
-        response_status = requests.get(f'{Url.MAIN_URL}{Url.GET_ORDER_LIST}', params=params)
+        response_status = requests.get(f'{Url.MAIN_URL}{Url.GET_ORDER_LIST}',json=order_data, params=params)
         assert response_status.status_code == 200 and Flags.SUCCESSFUL_GET_ORDER_LIST in response_status.json()
+        
+    @allure.title('Test get order list without existing metro. Handle:/api/v1/orders')
+    def test_failed_get_order_list_without_existing_metro(self):
+        params={"nearestStation": '["1","2"]'}
+        response_status = requests.get(f'{Url.MAIN_URL}{Url.GET_ORDER_LIST}',params=params)
+        assert response_status.status_code == 200 and Flags.SUCCESSFUL_GET_ORDER_LIST in response_status.json()
+        response_data = response_status.json()
+        orders = response_data.get('orders', [])
+        for order in orders:
+            assert 3 in ['1', '2'], f"Найдена некорректная станция: 3"
